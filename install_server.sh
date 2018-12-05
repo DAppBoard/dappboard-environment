6d68f55560b160622bfe6077e82c78f9e439c293
Jules Dourlens <jdourlens@gmail.com>
2018-12-05 15:04:32 +0400

    Cleaned install script

--- install_server.sh
+++ install_server.sh
@@ -4,17 +4,16 @@ curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
 
 #Install it
 bash nodesource_setup.sh
-sudo apt-get install nodejs
+apt-get install -y nodejs
 curl -L https://npmjs.org/install.sh | sudo sh
 
-
-apt-get install build-essential python
+# Python is needed for some of the nodejs modules
+apt-get install -y build-essential python git htop emacs-nox
 
 npm install -g pm2
 
 pm2 install pm2-logrotate
 
-apt-get install git htop emacs-nox
-
-# Clone the ETL repository
-git clone https://github.com/DAppBoard/ethereum-etl.git
+# Clone the ETL and Web repository
+git clone https://github.com/DAppBoard/dappboard-etl.git
+git clone https://github.com/DAppBoard/dappboard-web.git
